8044fe81cf5b1cd940fdc3663cd9bcfbc37928be
Eder Melo Gonzalez <coordinadorsistemas@CABLEEXPRESS.COM>
2024-04-26 16:50:18 -0500

    V1.0.0.5 - CORS all

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from src.routes.roles_route import roles
 from src.routes.tipoDoc_router import tipo_doc
 from src.routes.barrios_router import barrios
 from src.routes.login_router import login
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Agregar middleware para permitir CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar una lista de orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],  # Puedes especificar una lista de métodos permitidos (GET, POST, etc.)
+    allow_headers=["*"],  # Puedes especificar una lista de encabezados permitidos
+)
 
 app.include_router(inicial)
 app.include_router(usuarios)
